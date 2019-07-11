3c4ac429f52bdcdc367fc43fe5952a18fb908b19
Muhammad Nadat <muhammad.nadat@gmail.com>
2019-07-11 18:46:10 +0400

    basics working
    Added method-override package
    edit and delete functions now working

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
 
-/* GET books listing. */
+/* GET route for root (show all books) */
 router.get("/", function(req, res, next) {
   Book.findAll({}).then((books) => {
     res.render("books/index", { title: "Books", books:books });
@@ -10,20 +10,28 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/", function(req,res,next){
-  console.log("post at root");
+  Book.create(req.body).then(function(book){
+    res.redirect("/books/" + book.id);
+  });
 });
 
-/* Create a new book form. */
+// holder for pagination
+router.get("/page/:id", function(req,res,next){
+  console.log(`page ${req.params.id} of 10`);
+});
+
+/* GET router for new book */
 router.get("/new", function(req, res, next) {
   res.render("books/new", {book:Book.build(), title: "New Book"});
 });
 
-/* Edit a  book form. */
+/* GET route for edit book */
 router.get("/:id/edit", function(req,res,next){
   Book.findByPk(req.params.id).then(function(book){
     res.render("books/edit", {book: book, title: "Edit Book"});
   });
 });
+
 
 router.get("/:id/delete", function(req,res,next){
   Book.findByPk(req.params.id).then(function(book){
@@ -49,7 +57,15 @@ router.put("/:id", function(req,res,next){
 });
 
 router.delete("/:id", function(req,res,next){
-  console.log("time to delete");
+  Book.findByPk(req.params.id).then(function(book){
+    if(book) {
+      return book.destroy();
+    } else {
+      res.send(404);
+    }
+  }).then(function(){
+    res.redirect("/books"); 
+  });
 });
 
 
