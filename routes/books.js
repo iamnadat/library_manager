b596c8e9ca34d847f5792bcc5b8305fd9ebbd385
BeardedGeek <muhammad.nadat@gmail.com>
2019-07-13 02:21:06 +0400

    Errors v1
    error handelling

--- routes/books.js
+++ routes/books.js
@@ -1,21 +1,31 @@
+var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
 
-/* GET route for root (show all books) */
+/* GET route to show all books */
 router.get("/", function(req, res, next) {
   Book.findAll({}).then((books) => {
     res.render("books/index", { title: "Books", books:books });
   });
 });
 
+/* GET route to create a ne book */
 router.post("/", function(req,res,next){
   Book.create(req.body).then(function(book){
     res.redirect("/books/" + book.id);
+  }).catch(function(error){
+    if(error.name === "SequelizeValidationError") {
+      res.render("books/new",{book:Book.build(req.body),errors:error.errors, title:"New Book"});
+    } else {
+      throw error;
+    }
+  }).catch(function(error){
+    res.send(500,error);
   });
 });
 
-// holder for pagination
+/* holder for pagination*/
 router.get("/page/:id", function(req,res,next){
   console.log(`page ${req.params.id} of 10`);
 });
@@ -28,7 +38,14 @@ router.get("/new", function(req, res, next) {
 /* GET route for edit book */
 router.get("/:id/edit", function(req,res,next){
   Book.findByPk(req.params.id).then(function(book){
-    res.render("books/edit", {book: book, title: "Edit Book"});
+    if(book) {
+      res.render("books/edit", {book: book, title: "Edit Book"});
+    } else {
+      var err = createError(404, 'This book does not exist!');
+      res.render("error",{message:"Ooooops !!",error:err});
+    }
+  }).catch((error) => {
+    res.send(500,error);
   });
 });
 
@@ -42,17 +59,35 @@ router.get("/:id/delete", function(req,res,next){
 /* Show a  book form. */
 router.get("/:id", function(req,res,next) {
   Book.findByPk(req.params.id).then(function(book){
-    res.render("books/show",{book: book, title: book.title});
+    if(book) {
+      res.render("books/show",{book: book, title: book.title});
+    } else {
+      var err = createError(404, 'This book does not exist!');
+      res.render("error",{message:"Ooooops !!",error:err});
+    }    
   });
 });
 
 /* Update a  book using put. */
 router.put("/:id", function(req,res,next){
-  console.log("\n\n\nhere");
   Book.findByPk(req.params.id).then(function(book){
-    return book.update(req.body);
+    if(book) {
+      return book.update(req.body);
+    } else {
+      res.send(404);
+    }
   }).then((book) => {
     res.redirect("/books/" + book.id);
+  }).catch((error) => {
+    if(error.name === 'SequelizeValidationError') {
+      var book = Book.build(req.body);
+      book.id = req.params.id;
+      res.render("books/edit",{book:book,errors:error.errors,title:"Edit Book"});
+    } else {
+      throw error;
+    }
+  }).catch((error) => {
+    res.send(500, error);
   });
 });
 
