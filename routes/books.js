0870be5b7d81ff593154bba6f64e87efec4cde74
BeardedGeek <muhammad.nadat@gmail.com>
2019-07-19 23:54:08 +0400

    Pagination
    Pagination complete

--- routes/books.js
+++ routes/books.js
@@ -3,11 +3,16 @@ var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
 
+let currentPage;
+let limit = 5;
+let offset;
+
 /* GET route to show all books */
 router.get("/", function(req, res, next) {
-  Book.findAll({}).then((books) => {
-    res.render("books/index", { title: "Books", books:books });
-  });
+  res.redirect("/books/page/1")
+  // Book.findAll({offset: 30,limit: 2}).then((books) => {
+  //   res.render("books/index", { title: "Books", books:books });
+  // });
 });
 
 /* GET route to create a ne book */
@@ -27,22 +32,47 @@ router.post("/", function(req,res,next){
 
 /* holder for pagination*/
 router.get("/page/:id", function(req,res,next){
-  console.log(`page ${req.params.id} of 10`);
+  page =  parseInt(req.params.id);
+  if(isNaN(page)){
+    var error = createError(500, 'Page is not a number');
+    next(error);
+  } else{
+    offset = (page * limit) - limit;
+    currentPage = page;
+    Book.findAndCountAll({
+      offset: offset,
+      limit: limit
+    })
+    .then(books => {
+      if(books.rows.length > 0){
+        var noOfPages = Math.ceil(books.count / limit);
+        console.log(noOfPages);
+        res.render("books/index", { title: "Books", books:books.rows, noOfPages:noOfPages });
+      } else {
+        var error = createError(500, 'No books found');
+        next(error);
+      }
+    })
+    .catch((error) =>{
+      res.send(500,error);
+      next(error);
+    });
+  }
 });
 
 /* GET router for new book */
 router.get("/new", function(req, res, next) {
-  res.render("books/new", {book:Book.build(), title: "New Book"});
+  res.render("books/new", {book:Book.build(), title: "New Book", currentPage:currentPage});
 });
 
 /* GET route for edit book */
 router.get("/:id/edit", function(req,res,next){
   Book.findByPk(req.params.id).then(function(book){
     if(book) {
-      res.render("books/edit", {book: book, title: "Edit Book"});
+      res.render("books/edit", {book: book, title: "Edit Book", currentPage:currentPage});
     } else {
-      var err = createError(404, 'This book does not exist!');
-      res.render("error",{message:"Ooooops !!",error:err});
+      var error = createError(404, 'This book does not exist!');
+      res.render("error",{message:"Ooooops !!",error:error});
     }
   }).catch((error) => {
     res.send(500,error);
@@ -60,11 +90,11 @@ router.get("/:id/delete", function(req,res,next){
 router.get("/:id", function(req,res,next) {
   Book.findByPk(req.params.id).then(function(book){
     if(book) {
-      res.render("books/show",{book: book, title: book.title});
+      res.render("books/show",{book: book, title: book.title, currentPage:currentPage});
     } else {
-      var err = createError(404, 'This book does not exist!');
-      // res.render("error",{message:"Ooooops !!",error:err});
-      next(err);
+      var error = createError(404, 'This book does not exist!');
+      // res.render("error",{message:"Ooooops !!",error:error});
+      next(error);
     }    
   });
 });
@@ -83,7 +113,7 @@ router.put("/:id", function(req,res,next){
     if(error.name === 'SequelizeValidationError') {
       var book = Book.build(req.body);
       book.id = req.params.id;
-      res.render("books/edit",{book:book,errors:error.errors,title:"Edit Book"});
+      res.render("books/edit",{book:book,errors:error.errors,title:"Edit Book",currentPage:currentPage});
     } else {
       throw error;
     }
